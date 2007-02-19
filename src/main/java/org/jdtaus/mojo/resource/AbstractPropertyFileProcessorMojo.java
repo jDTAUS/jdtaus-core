ea2ca5949aaa7e8f070684d7e824e0399bd7aa61
Christian Schulte <cs@schulte.it>
2007-02-19 22:28:34 +0000

    - updated javadoc
    git-svn-id: http://svn.jdtaus.org/svnroot/jdtaus/jdtaus-core/jdtaus-resource-mojo/trunk@1649 d3f52c91-230f-0410-802c-e3ec080d4bae

--- src/main/java/org/jdtaus/mojo/resource/AbstractPropertyFileProcessorMojo.java
+++ src/main/java/org/jdtaus/mojo/resource/AbstractPropertyFileProcessorMojo.java
@@ -1,6 +1,6 @@
 /*
  *  jDTAUS - DTAUS fileformat.
- *  Copyright (C) 2005 - 2007 Christian Schulte <cs@schulte.it>
+ *  Copyright (c) 2005 Christian Schulte <cs@schulte.it>
  *
  *  This library is free software; you can redistribute it and/or
  *  modify it under the terms of the GNU Lesser General Public
