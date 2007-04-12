cab62e7611587ce7a4cecf8192d1ba14f32399d9
Christian Schulte <cs@schulte.it>
2007-04-12 09:18:53 +0000

    - fixed package
    git-svn-id: http://svn.jdtaus.org/svnroot/jdtaus/jdtaus-core/jdtaus-core-utilities/trunk@2520 d3f52c91-230f-0410-802c-e3ec080d4bae

--- src/test/java/org/jdtaus/core/io/util/test/StructuredFileOperationsTest.java
+++ src/test/java/org/jdtaus/core/io/util/test/StructuredFileOperationsTest.java
@@ -17,7 +17,7 @@
  *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  *
  */
-package org.jdtaus.core.io.spi.test;
+package org.jdtaus.core.io.util.test;
 
 import java.io.IOException;
 import org.jdtaus.core.io.StructuredFile;
