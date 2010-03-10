ff2880d94e4d720f5eff7f68da64008ed71e04c5
Christian Schulte <cs@schulte.it>
2010-03-10 18:17:13 +0000

    o Prepared for next development iteration. o Updated issue link template from https to http to stop Sourceforge from requiring a login.
    git-svn-id: http://svn.jdtaus.org/svnroot/jdtaus/jdtaus-core/branches/jdtaus-core-1.x@8408 d3f52c91-230f-0410-802c-e3ec080d4bae

--- jdtaus-core-utilities/src/main/java/org/jdtaus/core/sax/util/EntityResolverChain.java
+++ jdtaus-core-utilities/src/main/java/org/jdtaus/core/sax/util/EntityResolverChain.java
@@ -21,7 +21,9 @@
 package org.jdtaus.core.sax.util;
 
 import java.io.IOException;
+import java.util.Locale;
 import org.jdtaus.core.container.ContainerFactory;
+import org.jdtaus.core.logging.spi.Logger;
 import org.xml.sax.EntityResolver;
 import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
@@ -35,26 +37,6 @@ import org.xml.sax.SAXException;
  */
 public final class EntityResolverChain implements EntityResolver
 {
-    //--Dependencies------------------------------------------------------------
-
-// <editor-fold defaultstate="collapsed" desc=" Generated Code ">//GEN-BEGIN:jdtausDependencies
-    // This section is managed by jdtaus-container-mojo.
-
-    /**
-     * Gets the configured <code>DefaultResolvers</code> implementation.
-     *
-     * @return The configured <code>DefaultResolvers</code> implementation.
-     */
-    private EntityResolver[] getDefaultResolvers()
-    {
-        return (EntityResolver[]) ContainerFactory.getContainer().
-            getDependency( this, "DefaultResolvers" );
-
-    }
-
-// </editor-fold>//GEN-END:jdtausDependencies
-
-    //------------------------------------------------------------Dependencies--
     //--EntityResolver----------------------------------------------------------
 
     /**
@@ -79,11 +61,12 @@ public final class EntityResolverChain implements EntityResolver
      * @see EntityResolver#resolveEntity(java.lang.String, java.lang.String)
      */
     public InputSource resolveEntity( final String publicId,
-        final String systemId ) throws SAXException, IOException
+                                      final String systemId )
+        throws SAXException, IOException
     {
         InputSource inputSource = null;
-        for ( int i = this.getResolvers().length - 1; i >= 0 &&
-            inputSource == null; i-- )
+        for ( int i = this.getResolvers().length - 1;
+              i >= 0 && inputSource == null; i-- )
         {
             final EntityResolver resolver = this.getResolvers()[i];
             inputSource = resolver.resolveEntity( publicId, systemId );
@@ -124,7 +107,7 @@ public final class EntityResolverChain implements EntityResolver
     }
 
     /**
-     * Gets the resolvers of the instane.
+     * Gets the resolvers of the instance.
      *
      * @return the resolvers used for resolving entities.
      */
@@ -133,10 +116,85 @@ public final class EntityResolverChain implements EntityResolver
         if ( this.resolvers == null )
         {
             this.resolvers = this.getDefaultResolvers();
+
+            if ( this.resolvers.length == 0 )
+            {
+                this.getLogger().warn(
+                    this.getNoEntityResolversMessage( this.getLocale() ) );
+
+            }
         }
 
         return this.resolvers;
     }
 
     //-----------------------------------------------------EntityResolverChain--
+    //--Dependencies------------------------------------------------------------
+
+// <editor-fold defaultstate="collapsed" desc=" Generated Code ">//GEN-BEGIN:jdtausDependencies
+    // This section is managed by jdtaus-container-mojo.
+
+    /**
+     * Gets the configured <code>Logger</code> implementation.
+     *
+     * @return The configured <code>Logger</code> implementation.
+     */
+    private Logger getLogger()
+    {
+        return (Logger) ContainerFactory.getContainer().
+            getDependency( this, "Logger" );
+
+    }
+
+    /**
+     * Gets the configured <code>Locale</code> implementation.
+     *
+     * @return The configured <code>Locale</code> implementation.
+     */
+    private Locale getLocale()
+    {
+        return (Locale) ContainerFactory.getContainer().
+            getDependency( this, "Locale" );
+
+    }
+
+    /**
+     * Gets the configured <code>DefaultResolvers</code> implementation.
+     *
+     * @return The configured <code>DefaultResolvers</code> implementation.
+     */
+    private EntityResolver[] getDefaultResolvers()
+    {
+        return (EntityResolver[]) ContainerFactory.getContainer().
+            getDependency( this, "DefaultResolvers" );
+
+    }
+
+// </editor-fold>//GEN-END:jdtausDependencies
+
+    //------------------------------------------------------------Dependencies--
+    //--Messages----------------------------------------------------------------
+
+// <editor-fold defaultstate="collapsed" desc=" Generated Code ">//GEN-BEGIN:jdtausMessages
+    // This section is managed by jdtaus-container-mojo.
+
+    /**
+     * Gets the text of message <code>noEntityResolvers</code>.
+     * <blockquote><pre>Keine ''EntityResolver'' gefunden. Java XML-Parser öffnen standardmäßig reguläre - eventuell entfernte - URI-Verbindungen. Sie sollten mindestens eine ''EntityResolver''-Implementierung (z.B. jdtaus-core-entity-resolver-1.9.4-SNAPSHOT.jar) zur Verfügung stellen.</pre></blockquote>
+     * <blockquote><pre>No entity resolvers found. Standard Java XML parsers fall back to opening regular - possibly remote - URI connections. You should provide at least one ''EntityResolver'' implementation (e.g. jdtaus-core-entity-resolver-1.9.4-SNAPSHOT.jar).</pre></blockquote>
+     *
+     * @param locale The locale of the message instance to return.
+     *
+     * @return the text of message <code>noEntityResolvers</code>.
+     */
+    private String getNoEntityResolversMessage( final Locale locale )
+    {
+        return ContainerFactory.getContainer().
+            getMessage( this, "noEntityResolvers", locale, null );
+
+    }
+
+// </editor-fold>//GEN-END:jdtausMessages
+
+    //----------------------------------------------------------------Messages--
 }
