91c01d70fae428f978c19cd4d617fca1b9ed5d8c
Christian Schulte <cs@schulte.it>
2012-03-22 23:51:46 +0000

    o Added class 'org.jdtaus.core.messages.IllegalStringMessage'. o Added class 'org.jdtaus.core.messages.IllegalNumberMessage'. o Added property 'columns' to class 'org.jdtaus.core.text.util.SwingMessagePane'. o Added properties 'columns' and 'minimumTaskDurationMillis' to class 'org.jdtaus.core.monitor.util.SwingProgressMonitor'. o Prepared for '1.10'.
    git-svn-id: http://svn.jdtaus.org/svnroot/jdtaus/jdtaus-core/branches/jdtaus-core-1.x@8485 d3f52c91-230f-0410-802c-e3ec080d4bae

---
+++ jdtaus-core-messages/src/main/java/org/jdtaus/core/messages/IllegalNumberMessage.java
@@ -0,0 +1,198 @@
+/*
+ *  jDTAUS Core Messages
+ *  Copyright (c) 2005 Christian Schulte
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ *  $Id: Implementation.java.vm 5414 2008-10-27 10:42:09Z schulte2005 $
+ */
+package org.jdtaus.core.messages;
+
+import java.util.Locale;
+import org.jdtaus.core.container.ContainerFactory;
+import org.jdtaus.core.text.Message;
+
+/**
+ * Message stating that an illegal number was specified.
+ *
+ * @author Christian Schulte
+ * @version $Id: Implementation.java.vm 5414 2008-10-27 10:42:09Z schulte2005 $
+ * @since 1.10
+ */
+public final class IllegalNumberMessage extends Message
+{
+    //--IllegalNumberMessage----------------------------------------------------
+
+    /** Serial version UID for backwards compatibility with 1.0.x classes. */
+    private static final long serialVersionUID = 6578496762473822182L;
+
+    /**
+     * The invalid number.
+     * @serial
+     */
+    private Number invalidNumber;
+
+    /**
+     * The minimum required value.
+     * @serial
+     */
+    private Number minimum;
+
+    /**
+     * The maximum allowed value.
+     * @serial
+     */
+    private Number maximum;
+
+    /**
+     * Creates a new {@code IllegalNumberMessage} instance taking an invalid number, a minimum required value and a
+     * maximum allowed value.
+     *
+     * @param invalidNumber The invalid number or {@code null} if no such number is known.
+     * @param minimum The minimum required value or {@code null} if no such requirement exists.
+     * @param maximum The maximum allowed value or {@code null} if no such limit exists.
+     */
+    public IllegalNumberMessage( final Number invalidNumber, final Number minimum, final Number maximum )
+    {
+        super();
+        this.invalidNumber = invalidNumber;
+        this.minimum = minimum;
+        this.maximum = maximum;
+    }
+
+    //----------------------------------------------------IllegalNumberMessage--
+    //--Message-----------------------------------------------------------------
+
+    /**
+     * {@inheritDoc}
+     * <ul>
+     * <li>[0]: The invalid number or {@code null} if no such number is known.</li>
+     * <li>[1]: The minimum required value or {@code null} if no such requirement exists.</li>
+     * <li>[2]: The maximum allowed value or {@code null} if no such limit exists.</li>
+     * </ul>
+     */
+    public Object[] getFormatArguments( final Locale locale )
+    {
+        return new Object[]
+            {
+                this.invalidNumber, this.minimum, this.maximum
+            };
+
+    }
+
+    public String getText( final Locale locale )
+    {
+        final StringBuffer b = new StringBuffer( 128 );
+
+        if ( this.invalidNumber != null )
+        {
+            b.append( this.getIllegalValueMessage( locale, this.invalidNumber ) ).append( " " );
+        }
+
+        if ( this.minimum != null )
+        {
+            b.append( this.getIllegalMinimumValueMessage( locale, this.minimum ) ).append( " " );
+        }
+
+        if ( this.maximum != null )
+        {
+            b.append( this.getIllegalMaximumValueMessage( locale, this.maximum ) ).append( " " );
+        }
+
+
+        if ( b.length() > 0 )
+        {
+            b.setLength( b.length() - 1 );
+        }
+
+        return b.toString();
+    }
+
+    //-----------------------------------------------------------------Message--
+    //--Messages----------------------------------------------------------------
+
+// <editor-fold defaultstate="collapsed" desc=" Generated Code ">//GEN-BEGIN:jdtausMessages
+    // This section is managed by jdtaus-container-mojo.
+
+    /**
+     * Gets the text of message <code>illegalValue</code>.
+     * <blockquote><pre>Ungültiger Wert {0,number}.</pre></blockquote>
+     * <blockquote><pre>Illegal value {0,number}.</pre></blockquote>
+     *
+     * @param locale The locale of the message instance to return.
+     * @param value Illegal value.
+     *
+     * @return Information about an illegal value.
+     */
+    private String getIllegalValueMessage( final Locale locale,
+            final java.lang.Number value )
+    {
+        return ContainerFactory.getContainer().
+            getMessage( this, "illegalValue", locale,
+                new Object[]
+                {
+                    value
+                });
+
+    }
+
+    /**
+     * Gets the text of message <code>illegalMaximumValue</code>.
+     * <blockquote><pre>Größer als {0,number}.</pre></blockquote>
+     * <blockquote><pre>Greater than {0,number}.</pre></blockquote>
+     *
+     * @param locale The locale of the message instance to return.
+     * @param maximum Maximum allowed value.
+     *
+     * @return Information about an illegal maximum value.
+     */
+    private String getIllegalMaximumValueMessage( final Locale locale,
+            final java.lang.Number maximum )
+    {
+        return ContainerFactory.getContainer().
+            getMessage( this, "illegalMaximumValue", locale,
+                new Object[]
+                {
+                    maximum
+                });
+
+    }
+
+    /**
+     * Gets the text of message <code>illegalMinimumValue</code>.
+     * <blockquote><pre>Kleiner als {0,number}.</pre></blockquote>
+     * <blockquote><pre>Less than {0,number}.</pre></blockquote>
+     *
+     * @param locale The locale of the message instance to return.
+     * @param minimum Minimum required value.
+     *
+     * @return Information about an illegal minimum value.
+     */
+    private String getIllegalMinimumValueMessage( final Locale locale,
+            final java.lang.Number minimum )
+    {
+        return ContainerFactory.getContainer().
+            getMessage( this, "illegalMinimumValue", locale,
+                new Object[]
+                {
+                    minimum
+                });
+
+    }
+
+// </editor-fold>//GEN-END:jdtausMessages
+
+    //----------------------------------------------------------------Messages--
+}
---
+++ jdtaus-core-messages/src/main/java/org/jdtaus/core/messages/IllegalStringMessage.java
@@ -0,0 +1,244 @@
+/*
+ *  jDTAUS Core Messages
+ *  Copyright (c) 2005 Christian Schulte
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ *  $Id: Implementation.java.vm 5414 2008-10-27 10:42:09Z schulte2005 $
+ */
+package org.jdtaus.core.messages;
+
+import java.util.Locale;
+import org.jdtaus.core.container.ContainerFactory;
+import org.jdtaus.core.text.Message;
+
+/**
+ * Message stating that an illegal string was specified.
+ *
+ * @author Christian Schulte
+ * @version $Id: Implementation.java.vm 5414 2008-10-27 10:42:09Z schulte2005 $
+ * @since 1.10
+ */
+public final class IllegalStringMessage extends Message
+{
+    //--IllegalStringMessage----------------------------------------------------
+
+    /** Serial version UID for backwards compatibility with 1.10.x classes. */
+    private static final long serialVersionUID = -8783693360487171440L;
+
+    /**
+     * The invalid string.
+     * @serial
+     */
+    private String invalidString;
+
+    /**
+     * The invalid characters of the string.
+     * @serial
+     */
+    private char[] invalidCharacters;
+
+    /**
+     * The minimum required length.
+     * @serial
+     */
+    private Number minimumLength;
+
+    /**
+     * The maximum allowed length.
+     * @serial
+     */
+    private Number maximumLength;
+
+    /**
+     * Creates a new {@code IllegalStringMessage} instance taking an invalid string, an array of invalid characters,
+     * a minimum required length and a maximum allowed length.
+     *
+     * @param invalidString The invalid string or {@code null} if no such string is known.
+     * @param invalidCharacters The invalid characters or {@code null} if no such characters are known.
+     * @param minimumLength The minimum required length or {@code null} if no such requirement exists.
+     * @param maximumLength The maximum allowed length or {@code null} if no such limit exists.
+     */
+    public IllegalStringMessage( final String invalidString, final char[] invalidCharacters,
+                                 final Number minimumLength, final Number maximumLength )
+    {
+        super();
+        this.invalidString = invalidString;
+        this.invalidCharacters = invalidCharacters;
+        this.minimumLength = minimumLength;
+        this.maximumLength = maximumLength;
+    }
+
+    //----------------------------------------------------IllegalStringMessage--
+    //--Message-----------------------------------------------------------------
+
+    /**
+     * {@inheritDoc}
+     * <ul>
+     * <li>[0]: The invalid string or {@code null} if no such string is known.</li>
+     * <li>[1]: The invalid characters or {@code null} if no such characters are known.</li>
+     * <li>[2]: The minimum required length or {@code null} if no such requirement exists.</li>
+     * <li>[3]: The maximum allowed length or {@code null} if no such limit exists.</li>
+     * </ul>
+     */
+    public Object[] getFormatArguments( final Locale locale )
+    {
+        return new Object[]
+            {
+                this.invalidString, this.invalidCharacters, this.minimumLength, this.maximumLength
+            };
+
+    }
+
+    public String getText( final Locale locale )
+    {
+        final StringBuffer b = new StringBuffer( 128 );
+
+        if ( this.invalidString != null )
+        {
+            b.append( this.getIllegalStringMessage( locale, this.invalidString ) ).append( " " );
+        }
+
+        if ( this.invalidCharacters != null )
+        {
+            final StringBuffer c = new StringBuffer( this.invalidCharacters.length * 2 ).append( "[" );
+
+            for ( int i = 0, s0 = this.invalidCharacters.length; i < s0; i++ )
+            {
+                c.append( this.invalidCharacters[i] ).append( ", " );
+            }
+
+            c.setLength( c.length() - 2 );
+            c.append( "]" );
+
+            b.append( this.getIllegalCharactersMessage( locale, c.toString() ) ).append( " " );
+        }
+
+        if ( this.minimumLength != null )
+        {
+            b.append( this.getIllegalMinimumStringLengthMessage( locale, this.minimumLength ) ).append( " " );
+        }
+
+        if ( this.maximumLength != null )
+        {
+            b.append( this.getIllegalMaximumStringLengthMessage( locale, this.maximumLength ) ).append( " " );
+        }
+
+        if ( b.length() > 0 )
+        {
+            b.setLength( b.length() - 1 );
+        }
+
+        return b.toString();
+    }
+
+    //-----------------------------------------------------------------Message--
+    //--Messages----------------------------------------------------------------
+
+// <editor-fold defaultstate="collapsed" desc=" Generated Code ">//GEN-BEGIN:jdtausMessages
+    // This section is managed by jdtaus-container-mojo.
+
+    /**
+     * Gets the text of message <code>illegalString</code>.
+     * <blockquote><pre>Ungültige Zeichenkette ''{0}''.</pre></blockquote>
+     * <blockquote><pre>Illegal string ''{0}''.</pre></blockquote>
+     *
+     * @param locale The locale of the message instance to return.
+     * @param illegalString Illegal string.
+     *
+     * @return Information about an illegal string.
+     */
+    private String getIllegalStringMessage( final Locale locale,
+            final java.lang.String illegalString )
+    {
+        return ContainerFactory.getContainer().
+            getMessage( this, "illegalString", locale,
+                new Object[]
+                {
+                    illegalString
+                });
+
+    }
+
+    /**
+     * Gets the text of message <code>illegalMaximumStringLength</code>.
+     * <blockquote><pre>Mehr als {0,number} Zeichen.</pre></blockquote>
+     * <blockquote><pre>More than {0,number} characters.</pre></blockquote>
+     *
+     * @param locale The locale of the message instance to return.
+     * @param maximumLength Maximum allowed length.
+     *
+     * @return Information about an illegal maximum string length.
+     */
+    private String getIllegalMaximumStringLengthMessage( final Locale locale,
+            final java.lang.Number maximumLength )
+    {
+        return ContainerFactory.getContainer().
+            getMessage( this, "illegalMaximumStringLength", locale,
+                new Object[]
+                {
+                    maximumLength
+                });
+
+    }
+
+    /**
+     * Gets the text of message <code>illegalMinimumStringLength</code>.
+     * <blockquote><pre>Weniger als {0,number} Zeichen.</pre></blockquote>
+     * <blockquote><pre>Less than {0,number} characters.</pre></blockquote>
+     *
+     * @param locale The locale of the message instance to return.
+     * @param minimumLength Minimum required length.
+     *
+     * @return Information about an illegal minimum string length.
+     */
+    private String getIllegalMinimumStringLengthMessage( final Locale locale,
+            final java.lang.Number minimumLength )
+    {
+        return ContainerFactory.getContainer().
+            getMessage( this, "illegalMinimumStringLength", locale,
+                new Object[]
+                {
+                    minimumLength
+                });
+
+    }
+
+    /**
+     * Gets the text of message <code>illegalCharacters</code>.
+     * <blockquote><pre>Ungültige Zeichen {0}.</pre></blockquote>
+     * <blockquote><pre>Illegal characters {0}.</pre></blockquote>
+     *
+     * @param locale The locale of the message instance to return.
+     * @param illegalCharacters Illegal characters.
+     *
+     * @return Information about illegal characters.
+     */
+    private String getIllegalCharactersMessage( final Locale locale,
+            final java.lang.String illegalCharacters )
+    {
+        return ContainerFactory.getContainer().
+            getMessage( this, "illegalCharacters", locale,
+                new Object[]
+                {
+                    illegalCharacters
+                });
+
+    }
+
+// </editor-fold>//GEN-END:jdtausMessages
+
+    //----------------------------------------------------------------Messages--
+}
